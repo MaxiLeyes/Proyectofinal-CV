022cdd85f5df8d7110fb04b9fca3a76f31eeea45
MaxiLeyes <146690630+MaxiLeyes@users.noreply.github.com>
2023-10-02 01:40:40 -0300

    Add files via upload

---
+++ myJavaScript.js
@@ -0,0 +1,11 @@
+//botones introduccion
+function  redireccion(){
+ locacion.href - "https://www.behance.net/"
+}
+function  redireccion(){
+  locacion.href - "https://github.com/"
+ }
+ function  redireccion(){
+  locacion.href - "https://ar.linkedin.com/"
+ }
+ 
